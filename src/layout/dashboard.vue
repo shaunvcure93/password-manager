83ce89c4c4ed389a3444263eb01021b899f5f847
shaun <shaun.vcure@hotmail.com>
2024-12-17 16:55:54 +0800

    setup route child, dashboard home

---
+++ src/layout/dashboard.vue
@@ -0,0 +1,197 @@
+<template>
+    <div class="sidemenu">
+        <div class="logo">
+            <div></div>
+            <span>Company Name</span>
+        </div>
+        <div class="menu">
+            <div class="nav"><i class="fi fi-rr-menu-burger"></i></div>
+            <div class="mainbox">
+                <div class="title">CATEGORIES</div>
+                <div class="list">
+                    <div class="subbox">
+                        <div></div>
+                        <span>My Vault</span>
+                    </div>
+                    <div class="subbox">
+                        <div></div>
+                        <span>Breach Watch</span>
+                    </div>
+                    <div class="subbox">
+                        <div></div>
+                        <span>Deleted Items</span>
+                    </div>
+                </div>
+            </div>
+            <div class="mainbox">
+                <div class="title">SETTINGS</div>
+                <div class="list">
+                    <div class="subbox">
+                        <div></div>
+                        <span>General</span>
+                    </div>
+                    <div class="subbox">
+                        <div></div>
+                        <span>Import / Export</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <router-view />
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+.sidemenu
+{
+    display           : block;
+    position          : fixed;
+    width             : 280px;
+    top               : 0px;
+    left              : 0px;
+    box-sizing        : border-box;
+    -webkit-box-sizing: border-box;
+    padding           : 20px;
+
+    & > .logo
+    {
+        display : block;
+        position: relative;
+        width   : 100%;
+        overflow: hidden;
+        margin  : 0px 0px 20px 0px;
+
+        & > div
+        {
+            display             : block;
+            position            : relative;
+            float               : left;
+            width               : 38px;
+            height              : 38px;
+            border-radius       : 50%;
+            -webkit-border-rdius: 50%;
+            background          : #000000;
+            margin              : 0px 10px 0px 0px;
+        }
+        & > span
+        {
+            display    : block;
+            position   : relative;
+            float      : left;
+            height     : 38px;
+            line-height: 38px;
+            font-size  : 14px;
+            font-weight: 600;
+        }
+    }
+    & > .menu
+    {
+        display             : block;
+        position            : relative;
+        width               : 100%;
+        box-sizing          : border-box;
+        -webkit-box-sizing  : border-box;
+        border              : solid 1px #D9D9D9;
+        border-radius       : 10px;
+        -webkit-borde-radius: 10px;
+        padding             : 20px;
+
+        & > .nav
+        {
+            display              : block;
+            position             : absolute;
+            width                : 28px;
+            height               : 28px;
+            top                  : 12px;
+            right                : 12px;
+            z-index              : 1;
+            cursor               : pointer;
+            border-radius        : 8px;
+            -webkit-border-radius: 8px;
+            background           : rgba(0,0,0,0.1);
+
+            & > i
+            {
+                display    : block;
+                position   : absolute;
+                width      : 16px;
+                height     : 16px;
+                line-height: 16px;
+                text-align : center;
+                font-size  : 16px;
+                top        : calc(50% - 8px);
+                left       : calc(50% - 8px);
+            }
+        }
+        & .mainbox
+        {
+            display : block;
+            position: relative;
+            width   : 100%;
+            margin  : 0px 0px 24px 0px;
+
+            &:last-child
+            {
+                margin:0px;
+            }
+            & > .title
+            {
+                display    : block;
+                position   : relative;
+                width      : 100%;
+                font-size  : 12px;
+                font-weight: 600;
+                margin     : 0px 0px 14px 0px;
+            }
+            & > .list
+            {
+                display : block;
+                position: relative;
+                width   : 100%;
+
+                & .subbox
+                {
+                    display           : block;
+                    position          : relative;
+                    width             : 100%;
+                    box-sizing        : border-box;
+                    -webkit-box-sizing: border-box;
+                    padding           : 0px 0px 0px 34px;
+                    margin            : 0px 0px 8px 0px;
+                    cursor            : pointer;
+
+                    &:last-child
+                    {
+                        margin:0px;
+                    }
+                    & > div
+                    {
+                        display              : block;
+                        position             : absolute;
+                        width                : 24px;
+                        height               : 24px;
+                        border-radius        : 8px;
+                        -webkit-border-radius: 8px;
+                        background           : rgba(0,0,0,0.1);
+                        top                  : 0;
+                        left                 : 0;
+                    }
+                    & > span
+                    {
+                        display    : block;
+                        position   : relative;
+                        width      : 100%;
+                        height     : 24px;
+                        line-height: 24px;
+                        font-size  : 14px;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import login from './pages/login.vue';
 import error from './pages/error.vue';
 import register from './pages/register.vue';
 import forget from './pages/forget.vue';
+import home from './pages/home.vue';
+import dashboard from './layout/dashboard.vue';
 
 export const history = createWebHashHistory()
 export const router = createRouter({
@@ -21,12 +23,23 @@ export const router = createRouter({
       component: forget
     },
     {
+      path: '/',
+      redirect: { path: '/home' }, // redirect property 
+      component: dashboard,
+      children: [
+        {
+          path: '/home',
+          component: home,
+        },
+      ],
+    },
+    {
       path: '/error',
       component: error
     },
-    {
-      path: '/:pathMatch(.*)*',
-      redirect: '/error'
-    }
+    // {
+    //   path: '/:pathMatch(.*)*',
+    //   redirect: '/error'
+    // }
   ]
 })
